5e837eba3a5571f6ec988d52ac2150be6a147281
Karina <not-funny@mail.ru>
2023-07-28 13:08:19 +0700

    reformat code

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,6 +17,7 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
     @Test
     void findNewMax() {
         StatisticsService service = new StatisticsService();
